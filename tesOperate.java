0617464837223265a533f8e0dcb74d005c4f8ef1
Pharatee018 <152241498+Pharatee018@users.noreply.github.com>
2024-07-25 14:41:05 +0700

    Create tesOperate.java

---
+++ tesOperate.java
@@ -0,0 +1,9 @@
+public class tesOperate {
+    public static void main(String[] args) {
+        int x, y;
+        x = 5 + 2 * 4;
+        y = (5 + 2) * 4;
+        System.out.println("x = " +x);
+        System.out.println("y = " +y);
+    }
+}
